bc13c0075bfb33f8a024a1daafedd43c7ebfcb29
ryuk878 <J.hartsink@gmail.com>
2021-12-19 17:26:04 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+
+
+
+const container = document.querySelector('.fireworks-example')
+const fireworks = new Fireworks(container, {
+      rocketsPoint: 75,
+      hue: { min: 0, max: 360 },
+      delay: { min: 15, max: 30 },
+      speed: 2,
+      acceleration: 1.05,
+      gravity: 1.5,
+      particles: 130,
+      trace: 3,
+      explosion: 4,
+      autoresize: true,
+      brightness: { 
+        min: 50, 
+        max: 100,
+        decay: { min: 0.015, max: 0.03 }
+      },
+      mouse: { 
+        click: false, 
+        move: false, 
+        max: 3 
+      },
+      boundaries: { 
+        
+        x: 50, 
+        y: 50, 
+        width: container.clientWidth, 
+        height: container.clientHeight 
+      },
+      sound: {
+        enable: true,
+        files: [
+          'explosion0.mp3',
+          'explosion1.mp3',
+          'explosion2.mp3'
+        ],
+        volume: { min: 1, max: 2 },
+      }
+      
+});
+
+
+
+setTimeout(() => {
+    fireworks.start()
+},5000)
+
+setTimeout(() => {
+    fireworks.stop()
+},15000)
+
+
+
